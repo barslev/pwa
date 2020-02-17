a8ee2cf89a87433b7c4732a44f65790bb96a6f00
Aaron Martin <aaron.martin@shopgate.com>
2020-02-17 12:25:53 -0600

    PWA-2476: undo last mislayed commit

--- libraries/engage/components/constants.js
+++ libraries/engage/components/constants.js
@@ -1,3 +1,2 @@
 export const PORTAL_PRODUCT_IMAGE = 'component.product-image';
 export const PORTAL_PRODUCT_MEDIA = 'component.product-media';
-export const PORTAL_PRODUCT_MEDIA_SECTION = 'component.product-media-section';
